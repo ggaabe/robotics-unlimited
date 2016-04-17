5c08d1a05196dceb861059aa6be16507e69761ca
ggaabe <ggaabe@gmail.com>
2016-04-16 23:46:11 -0400

    Successfully implemented feature 1, user can now change background as well as move arms.

--- Robotics Unlimited/GameViewController.swift
+++ Robotics Unlimited/GameViewController.swift
@@ -11,14 +11,26 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
+    let backgroundArray = ["art.scnassets/ship.scn", "art.scnassets/spheres.scn", "art.scnassets/squares.scn"]
+    var backgroundIndex : Int = 0
+    
     override func viewDidLoad() {
             super.viewDidLoad()
-            
+            print(screenSize)
             let scnView = self.view as! SCNView
-            scnView.scene = MyScene() 
-            scnView.backgroundColor = UIColor.blackColor()
+            scnView.scene = MyScene()
+            MyScene().initializeRobot((scnView.scene?.rootNode)!)
+
+            scnView.scene = SCNScene(named: backgroundArray[backgroundIndex])! //MyScene()
+            MyScene().initializeRobot((scnView.scene?.rootNode)!)
+            
+            scnView.pointOfView = scnView.scene?.rootNode.childNodeWithName("CAMERA", recursively: false)
+            print(scnView.scene?.rootNode.childNodes)
+            //just find the cameranode and reasign its original vector to it
         
+            //scnView.backgroundColor = UIColor.blackColor()
+            
 //        super.viewDidLoad()
         
 //        // create a new scene
@@ -71,81 +83,107 @@ class GameViewController: UIViewController {
 //        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
 //        scnView.addGestureRecognizer(tapGesture)
         scnView.autoenablesDefaultLighting = true
+        
+        print("test")
+        let armMovementRecognizer = UIPanGestureRecognizer(target: self, action: #selector(GameViewController.handleArmMovement(_:)))
+        scnView.addGestureRecognizer(armMovementRecognizer)
+
         //scnView.allowsCameraControl = true
-       // SCNAction.
+       
     }
     
-//    func handleTap(gestureRecognize: UIGestureRecognizer) {
-//        // retrieve the SCNView
-//        let scnView = self.view as! SCNView
-//        
-//        // check what nodes are tapped
-//        let p = gestureRecognize.locationInView(scnView)      //pass a robot arm to locationInView.
-//
-//        let hitResults = scnView.hitTest(p, options: nil)   //pass robotarm to hittest, rotateOnAxis
-//        // check that we clicked on at least one object
-//        if hitResults.count > 0 {
-//            // retrieved the first clicked object
-//            let result: AnyObject! = hitResults[0]
-//            
-//            // get its material
-//            let material = result.node!.geometry!.firstMaterial!
-//            
-//            // highlight it
-//            SCNTransaction.begin()
-//            SCNTransaction.setAnimationDuration(0.5)
-//            
-//            // on completion - unhighlight
-//            SCNTransaction.setCompletionBlock {
-//                SCNTransaction.begin()
-//                SCNTransaction.setAnimationDuration(0.5)
-//                
-//                material.emission.contents = UIColor.blackColor()
-//                
-//                SCNTransaction.commit()
-//            }
-//            
-//            material.emission.contents = UIColor.redColor()
-//            
-//            SCNTransaction.commit()
-//        }
-//    }
+    var previousY :CGFloat = 0
+    var initialY :CGFloat = 0
+    func handleArmMovement(gestureRecognize: UIGestureRecognizer){
+         //retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        let p = gestureRecognize.locationInView(scnView)
+        let hitResults = scnView.hitTest(p, options: nil)
+        
+        print("Position: " + String(p.y))
+        
+        let velocity = (p.y - previousY)
+        
+        print(velocity)
+        
+        previousY = p.y
+        
+        print(hitResults.count)
+        if(gestureRecognize.state == UIGestureRecognizerState.Began){
+            initialY = p.y
+        }
+        else if(gestureRecognize.state == UIGestureRecognizerState.Ended){
+            if((p.y / screenSize.height) > 0.84 && (initialY / screenSize.height) > 0.84){
+                if(backgroundIndex+1 == backgroundArray.count){
+                    backgroundIndex = 0
+                }else{
+                    backgroundIndex += 1
+                }
+                scnView.scene = SCNScene(named: backgroundArray[backgroundIndex])!
+                MyScene().initializeRobot((scnView.scene?.rootNode)!)
+            }
+        }
+        if(hitResults.count > 0){
+            let result: SCNHitTestResult! = hitResults[0]
+            
+//            let material = result.node.geometry!.firstMaterial!
+            print(String((p.y / screenSize.height)))
+            print(result.node.name)
+            if(result.node.name == "LEFTARM" || result.node.name == "RIGHTARM"){
+                let rotateAction = SCNAction.rotateByAngle(velocity/100, aroundAxis: SCNVector3(x: 2.0, y: 0.0, z: 0.0), duration: 0.0)
+                //I CAN POSSIBLY CHANGE THE PIVOT
+                result.node.runAction(rotateAction)
+                
+                /*scnView.scene = SCNScene(named: "art.scnassets/ship.scn")!
+                MyScene().initializeRobot((scnView.scene?.rootNode)!)*/
+                
+                
+            }
+        }else{
     
-//    func handleTap(gestureRecognize: UIGestureRecognizer) {
-//        // retrieve the SCNView
-//        let scnView = self.view as! SCNView
-//        
-//        // check what nodes are tapped
-//        let p = gestureRecognize.locationInView(scnView)      //pass a robot arm to locationInView.
-//        
-//        let hitResults = scnView.hitTest(p, options: nil)   //pass robotarm to hittest, rotateOnAxis
-//        // check that we clicked on at least one object
-//        if hitResults.count > 0 {
-//            // retrieved the first clicked object
-//            let result: AnyObject! = hitResults[0]
-//            
-//            // get its material
-//            let material = result.node!.geometry!.firstMaterial!
-//            
-//            // highlight it
-//            SCNTransaction.begin()
-//            SCNTransaction.setAnimationDuration(0.5)
-//            
-//            // on completion - unhighlight
-//            SCNTransaction.setCompletionBlock {
-//                SCNTransaction.begin()
-//                SCNTransaction.setAnimationDuration(0.5)
-//                
-//                material.emission.contents = UIColor.blackColor()
-//                
-//                SCNTransaction.commit()
-//            }
-//            
-//            material.emission.contents = UIColor.redColor()
-//            
-//            SCNTransaction.commit()
-//        }
-//    }
+        }
+        // check what nodes are tapped
+             //   let p = gestureRecognize.locationInView(scnView)   //pass a robot arm to locationInView.
+        
+        
+    }
+    
+    func handleTap(gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.locationInView(scnView)      //pass a robot arm to locationInView.
+        
+        let hitResults = scnView.hitTest(p, options: nil)   //pass robotarm to hittest, rotateOnAxis
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result: AnyObject! = hitResults[0]
+            
+            // get its material
+            let material = result.node!.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.setAnimationDuration(0.5)
+            
+            // on completion - unhighlight
+            SCNTransaction.setCompletionBlock {
+                SCNTransaction.begin()
+                SCNTransaction.setAnimationDuration(0.5)
+                
+                material.emission.contents = UIColor.blackColor()
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.redColor()
+            
+            SCNTransaction.commit()
+        }
+    }
 
     
 //    override func shouldAutorotate() -> Bool {
--- Robotics Unlimited/MyScene.swift
+++ Robotics Unlimited/MyScene.swift
@@ -12,18 +12,67 @@ import SceneKit
 class MyScene : SCNScene {
     override init() {
         super.init()
-//        let plane = SCNPlane(width: 1.0, height: 1.0)
-//        plane.firstMaterial?.diffuse.contents = UIColor.blueColor()
-//
-//        let planeNode = SCNNode(geometry: plane)
-//        
-//        rootNode.addChildNode(planeNode)
         
+//        let sphere = SCNSphere(radius: 0.4)
+//        sphere.firstMaterial?.diffuse.contents = UIColor.greenColor()
+//        let sphereNode = SCNNode(geometry: sphere)
+//        sphereNode.position = SCNVector3(x: 0.0, y: 1.8, z: 0.0)
+//        rootNode.addChildNode(sphereNode)
+//        
+//        let leftLeg = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
+//        leftLeg.firstMaterial?.diffuse.contents = UIColor.blueColor()
+//        let leftLegNode = SCNNode(geometry: leftLeg)
+//        leftLegNode.position = SCNVector3(x: -0.3, y: 0.1, z: 0.0)
+//        
+//        let rightLeg = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
+//        rightLeg.firstMaterial?.diffuse.contents = UIColor.redColor()
+//        let rightLegNode = SCNNode(geometry: rightLeg)
+//        rightLegNode.position = SCNVector3(x: 0.3, y: 0.1, z: 0)
+//        
+//        let torso = SCNBox(width: 1.0, height: 1.0, length: 0.5, chamferRadius: 0)
+//        torso.firstMaterial?.diffuse.contents = UIColor.redColor()
+//        let torsoNode = SCNNode(geometry: torso)
+//        torsoNode.position = SCNVector3(x: 0.0, y: 1.0, z: 0.0)
+//        
+//        
+//        
+//        //IDEA: Control arms by sliding vertically up and down. They can rotate along a specific axis.
+//        //Arms will rotate around their center. Therefore they should be made bigger and pushed further away, or a way must be found to rotate them around their base.
+//        let leftArm = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
+//        leftArm.firstMaterial?.diffuse.contents = UIColor.greenColor()
+//        let leftArmNode = SCNNode(geometry: leftArm)
+//        leftArmNode.position = SCNVector3(x: -0.7, y: 1.0, z: 0.0)
+//        leftArmNode.name = "LEFTARM"
+//        
+//        let rightArm = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
+//        rightArm.firstMaterial?.diffuse.contents = UIColor.greenColor()
+//        let rightArmNode = SCNNode(geometry: rightArm)
+//        rightArmNode.position = SCNVector3(x: 0.7, y: 1.0, z: 0)
+//        rightArmNode.name = "RIGHTARM"
+//        
+//        let cameraNode = SCNNode()
+//        cameraNode.camera = SCNCamera()
+//        cameraNode.eulerAngles = SCNVector3(x: -0.5, y: 0.0, z: 0.0)
+//        
+//        rootNode.addChildNode(cameraNode)
+//
+//        // place the camera
+//        cameraNode.position = SCNVector3(x: 0, y: 2.3, z: 3)
+//        
+//        rootNode.addChildNode(cameraNode)
+//        rootNode.addChildNode(leftLegNode)
+//        rootNode.addChildNode(rightLegNode)
+//        rootNode.addChildNode(torsoNode)
+//        rootNode.addChildNode(leftArmNode)
+//        rootNode.addChildNode(rightArmNode)
+        
+    }
+    
+    func initializeRobot(newSceneRoot: SCNNode) {
         let sphere = SCNSphere(radius: 0.4)
-        sphere.firstMaterial?.diffuse.contents = UIColor.redColor()
+        sphere.firstMaterial?.diffuse.contents = UIColor.greenColor()
         let sphereNode = SCNNode(geometry: sphere)
         sphereNode.position = SCNVector3(x: 0.0, y: 1.8, z: 0.0)
-        rootNode.addChildNode(sphereNode)
         
         let leftLeg = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
         leftLeg.firstMaterial?.diffuse.contents = UIColor.blueColor()
@@ -40,36 +89,39 @@ class MyScene : SCNScene {
         let torsoNode = SCNNode(geometry: torso)
         torsoNode.position = SCNVector3(x: 0.0, y: 1.0, z: 0.0)
         
+        
+        
+        //IDEA: Control arms by sliding vertically up and down. They can rotate along a specific axis.
+        //Arms will rotate around their center. Therefore they should be made bigger and pushed further away, or a way must be found to rotate them around their base.
         let leftArm = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
-        leftArm.firstMaterial?.diffuse.contents = UIColor.blueColor()
+        leftArm.firstMaterial?.diffuse.contents = UIColor.greenColor()
         let leftArmNode = SCNNode(geometry: leftArm)
         leftArmNode.position = SCNVector3(x: -0.7, y: 1.0, z: 0.0)
+        leftArmNode.pivot = SCNMatrix4MakeTranslation(0.0, -0.0, 0.0)
+        leftArmNode.name = "LEFTARM"
         
         let rightArm = SCNBox(width: 0.2, height: 1.0, length: 0.2, chamferRadius: 0)
-        rightArm.firstMaterial?.diffuse.contents = UIColor.redColor()
+        rightArm.firstMaterial?.diffuse.contents = UIColor.greenColor()
         let rightArmNode = SCNNode(geometry: rightArm)
         rightArmNode.position = SCNVector3(x: 0.7, y: 1.0, z: 0)
+        rightArmNode.name = "RIGHTARM"
         
         let cameraNode = SCNNode()
+        cameraNode.name = "CAMERA"
         cameraNode.camera = SCNCamera()
         cameraNode.eulerAngles = SCNVector3(x: -0.5, y: 0.0, z: 0.0)
-        
-        rootNode.addChildNode(cameraNode)
-
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 1.8, z: 3)
+        cameraNode.position = SCNVector3(x: 0, y: 2.6/*2.3*/, z: 2.7)
         
-        rootNode.addChildNode(cameraNode)
-        rootNode.addChildNode(leftLegNode)
-        rootNode.addChildNode(rightLegNode)
-        rootNode.addChildNode(torsoNode)
-        rootNode.addChildNode(leftArmNode)
-        rootNode.addChildNode(rightArmNode)
+        newSceneRoot.addChildNode(leftLegNode)
+        newSceneRoot.addChildNode(rightLegNode)
+        newSceneRoot.addChildNode(torsoNode)
+        newSceneRoot.addChildNode(leftArmNode)
+        newSceneRoot.addChildNode(rightArmNode)
+        newSceneRoot.addChildNode(sphereNode)
         
-    }
-    
-    func initializeRobot(/*Pass a scene as a parameter to attach robot nodes to*/) {
-        
+        newSceneRoot.addChildNode(cameraNode)
+
     }
     
     required init(coder aDecoder: NSCoder) {
